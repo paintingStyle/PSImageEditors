4a41b763b998cda1593fdf5c0286b67b4568451d
rsf <rsf@rsf-iMac.local>
2018-08-24 11:35:54 +0800

    init project

---
+++ Example/PSImageEditors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PSImageEditors
+//
+//  Created by rsf on 2018/8/24.
+//  Copyright © 2018年 paintingStyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/PSImageEditors/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PSImageEditors
+//
+//  Created by rsf on 2018/8/24.
+//  Copyright © 2018年 paintingStyle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
